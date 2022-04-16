bad5e179a334feceede53269fbbdb1c02f48731a
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-04-16 12:33:31 +0000

    add join table

--- bouraq/view/dashboard.php
+++ bouraq/view/dashboard.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Trip</title>
+    <title>dashboard</title>
 </head>
 <body>
-
-    
-    
+    <h1>dashboard</h1>
+    <a href="http://localhost/youcode/back-end/client/LogOut"> 
+        Log Out
+    </a>
 </body>
 </html>
